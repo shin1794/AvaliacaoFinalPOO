fc03969762a0ec86848f74ec5ffd03cc46272cc3
Yi Shin <111324492+shin1794@users.noreply.github.com>
2023-10-12 11:26:35 -0300

    Add files via upload

---
+++ ClinicaVeterinaria/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import model.Animal;
+import model.Clinica;
+import model.Cao;
+import model.Gato;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ ClinicaVeterinaria/src/model/Clinica.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+public class Clinica {
+
+
+    private List<Animal> lista = new ArrayList<>();
+    private List<Animal> historicoMedico = new ArrayList<>();
+
+
+}
+
